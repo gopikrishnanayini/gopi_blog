9af423afc1d6d795c5c3e06628a80dc50216b103
gopikrishna <nayinigopi93@gmail.com>
2017-04-13 22:39:17 +0530

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20170412134536) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "gopis", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
